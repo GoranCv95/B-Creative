3a5c6ba9f7f78b1e49002d8fbaa57d66b6a9145d
GoranCv95 <gorancv95@gmail.com>
2022-11-10 00:59:24 +0100

    Almost mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-'use strict';
+"use strict";
 
 // Counter
 
-let valueDisplays = document.querySelectorAll('.counter-number');
+const sectionHeroEl = document.querySelector(".section-portfolio");
+
+let valueDisplays = document.querySelectorAll(".counter-number");
 let interval = 3000;
 
-valueDisplays.forEach(valueDisplay => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === true) {
+      valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
 
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function(){
-       startValue += 1;
-       valueDisplay.textContent = startValue;
-       if(startValue === endValue) {
-        clearInterval(counter)
-       }
-    }, duration);
-});
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function () {
+          startValue += 1;
+          valueDisplay.textContent = startValue;
+          if (startValue === endValue) {
+            clearInterval(counter);
+          }
+        }, duration);
+      });
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-280px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+const hoverEffect = document.querySelectorAll(".about-icon");
+const icons = document.querySelectorAll(".helper");
+const titles = document.querySelectorAll(".heading-secondary");
+const lines = document.querySelectorAll(".small-line");
+
+
