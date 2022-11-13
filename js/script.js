7d6745170bd9af0ca88a8281c6c49242595e70e0
GoranCv95 <gorancv95@gmail.com>
2022-11-13 22:29:08 +0100

    Make some progress with JS

--- js/script.js
+++ js/script.js
@@ -33,3 +33,85 @@ document.querySelectorAll(".main-nav-link").forEach((n) =>
     headerEl.classList.remove("nav-open");
   })
 );
+
+//
+const logos = document.querySelectorAll('.logos');
+const designes = document.querySelectorAll('.design');
+const developments = document.querySelectorAll('.development');
+const opti = document.querySelector('.opti');
+
+const btnAll = document.getElementById('all');
+const btnLogo = document.getElementById('logo');
+const btnDesign = document.getElementById('design');
+const btnDevelopment = document.getElementById('development');
+
+const grid = document.querySelector('.grid-wrapper');
+
+btnLogo.addEventListener('mouseenter', function(){
+  designes.forEach(img => img.style.display = 'none');
+  developments.forEach(img => img.style.display = 'none');
+  opti.style.display = 'none';
+  grid.classList.remove('grid-wrapper');
+  grid.style.display = 'flex';
+  grid.classList.add('grid-3-col');
+})
+
+btnLogo.addEventListener('mouseleave', function(){
+  designes.forEach(img => img.style.display = 'block');
+  developments.forEach(img => img.style.display = 'block');
+  opti.style.display = 'block';
+  grid.classList.remove('grid-3-col');
+  grid.style.display = 'grid';
+  grid.classList.add('grid-wrapper');
+})
+
+btnDesign.addEventListener('mouseover', function(){
+  logos.forEach(img => img.style.display = 'none');
+  developments.forEach(img => img.style.display = 'none');
+  opti.style.display = 'none';
+  grid.classList.remove('grid-wrapper');
+  grid.style.display = 'flex';
+  grid.classList.add('grid-2-col');
+})
+
+btnDesign.addEventListener('mouseleave', function(){
+  logos.forEach(img => img.style.display = 'block');
+  developments.forEach(img => img.style.display = 'block');
+  opti.style.display = 'block';
+  grid.classList.remove('grid-2-col');
+  grid.style.display = 'grid';
+  grid.classList.add('grid-wrapper');
+})
+
+btnDevelopment.addEventListener('mouseover', function(){
+  designes.forEach(img => img.style.display = 'none');
+  logos.forEach(img => img.style.display = 'none');
+  opti.style.display = 'none';
+  grid.classList.remove('grid-wrapper');
+  grid.style.display = 'flex';
+  grid.style.directions = 'row';
+  // grid.style.height = '10rem';
+  grid.classList.add('grid-2-col');
+})
+
+btnDevelopment.addEventListener('mouseleave', function(){
+  designes.forEach(img => img.style.display = 'block');
+  logos.forEach(img => img.style.display = 'block');
+  opti.style.display = 'block';
+  grid.classList.remove('grid-2-col');
+  grid.style.display = 'grid';
+  grid.classList.add('grid-wrapper');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
